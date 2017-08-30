378436a8aa87c1aaa3c1ddc2ab591364b0742323
Andrew Wybo <Andrew Wybo>
2017-08-29 21:24:19 -0400

    added questions in html and working submit button

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+    function handleClick() {         
+        var numCorrect = 0;   
+        
+        for(var i = 1; i <= 5; i++) {
+            var radios = document.getElementsByName('group'+i);
+        for(var j = 0; j < radios.length; j++) {
+            var radio = radios[j];
+        if(radio.value == "true" && radio.checked) {
+            numCorrect++;
+        }
+        }
+        }                   
+        alert("Correct Answers: " + numCorrect);
+    }
